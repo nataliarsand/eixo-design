6ca1885928956fcfbd23d046a0780aa260562863
Natalia Arsand <nataliarsand@gmail.com>
2025-05-09 23:21:08 +0200

    removing header paddings

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // replace with your actual repo name
+  base: '/eixo-design/', // IMPORTANT: match your repo name!
+  plugins: [react()],
 })
