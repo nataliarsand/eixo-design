c38b71cec4529a105a4f5d9d2c043befe51a6eae
Natalia Arsand <nataliarsand@gmail.com>
2025-05-09 23:49:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/eixo-design/', // IMPORTANT: match your repo name!
   plugins: [react()],
 })
